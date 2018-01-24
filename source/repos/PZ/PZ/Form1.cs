7e7f374fe4d849030d27bd74e97a86edaa2d3bfd
pww24 <pawel.wach4@gmail.com>
2018-01-24 13:54:37 +0100

    pierwszy szkielet aplikacji

--- source/repos/PZ/PZ/Form1.cs
+++ source/repos/PZ/PZ/Form1.cs
@@ -7,14 +7,81 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
 
 namespace PZ
 {
     public partial class Form1 : Form
     {
+        private TcpListener serwer;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private async void button1_Click_1(object sender, EventArgs e)
+        {
+            IPAddress adresIP;
+
+            // sprawdzanie czy adres IP został wpisany poprawnie
+            try
+            {
+                adresIP = IPAddress.Parse(textBox1.Text);
+            }
+            catch
+            {
+                textBox1.Text = String.Empty;
+                MessageBox.Show("Niewłaściwy format adresu IP", "Błąd adresu IP", MessageBoxButtons.OK);
+                return;
+            }
+            //
+
+            int port = Convert.ToInt16(numericUpDown1.Value); // konwertowanie portu
+
+
+            // wysyłanie wpisanego komunikatu z danego adresu IP oraz portu
+            try
+            {
+                serwer = new TcpListener(adresIP, port);
+                serwer.Start();
+                listBox1.Items.Add("komunikat wysyłany...");
+                listBox1.Update();
+
+                button1.Enabled = false;
+                button2.Enabled = true;
+
+                Socket newsocket = await serwer.AcceptSocketAsync(); //akceptacja klienta na kanale komunikacyjnym
+                if (newsocket.Connected)
+                {
+                    NetworkStream strumien = new NetworkStream(newsocket);
+                    byte[] buf = Encoding.ASCII.GetBytes(textBox2.Text); //string na kod ASCII
+                    strumien.Write(buf, 0, buf.Length);
+
+                    strumien.Flush(); //wyzwolenie transmisji
+                    strumien.Close();
+                }
+                newsocket.Close(); //zrywamy połączenie z klientem
+                listBox1.Items.Add("Komunikat został wysłany.");
+                listBox1.Update();
+                serwer.Stop();
+            }
+            catch
+            {
+                listBox1.Items.Add("Błąd transmisji");
+                listBox1.Update();
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            serwer.Stop();
+            button1.Enabled = true;
+            button2.Enabled = false;
+            textBox2.Text = string.Empty;
         }
     }
 }
